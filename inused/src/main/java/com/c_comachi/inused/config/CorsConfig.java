750b87524f1d5c7e636098fc552e1d6246f37078
MoonJaeGyeong <bblbblan69@gmail.com>
2024-01-28 14:48:47 +0900

    <update> : <CorsConfig update>

--- inused/src/main/java/com/c_comachi/inused/config/CorsConfig.java
+++ inused/src/main/java/com/c_comachi/inused/config/CorsConfig.java
@@ -1,17 +1,24 @@
 package com.c_comachi.inused.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
 
 @Configuration
-public class CorsConfig implements WebMvcConfigurer {
+public class CorsConfig {
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
 
-    @Override
-    public void addCorsMappings (CorsRegistry corsRegistry) {
-        corsRegistry
-                .addMapping("/**")
-                .allowedMethods("*")
-                .allowedOrigins("*");
+        source.registerCorsConfiguration("/api/**", config);
+        return new CorsFilter(source);
     }
 }
