d669396881d27d81bb117a8960e450e19eee67b0
MoonJaeGyeong <bblbblan69@gmail.com>
2024-01-28 14:52:34 +0900

    <feat> : <create RefreshTokenRepository>

---
+++ inused/src/main/java/com/c_comachi/inused/repository/RefreshTokenRepository.java
@@ -0,0 +1,10 @@
+package com.c_comachi.inused.repository;
+
+import com.c_comachi.inused.entity.RefreshToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
+    Optional<RefreshToken> findByKey(String key);
+}
