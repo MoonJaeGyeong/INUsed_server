c4f401fa81894d188012adc49ca162defae94a47
MoonJaeGyeong <bblbblan69@gmail.com>
2024-01-28 14:55:22 +0900

    <feat> : <add function findByEmail>

--- inused/src/main/java/com/c_comachi/inused/repository/UserRepository.java
+++ inused/src/main/java/com/c_comachi/inused/repository/UserRepository.java
@@ -4,9 +4,12 @@ import com.c_comachi.inused.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
 
+    Optional<UserEntity> findByEmail(String email);
     boolean existsByEmail(String email);
     boolean existsByNickname(String nickname);
 
