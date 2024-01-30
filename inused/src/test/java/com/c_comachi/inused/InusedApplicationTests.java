37dbd4e5712a16a5297d168e6993d37ed80bf7a9
MoonJaeGyeong <bblbblan69@gmail.com>
2024-01-31 05:28:28 +0900

    🚑 fix : ec2 에서 빌드 실패 뜨는 원인 제거

--- inused/src/test/java/com/c_comachi/inused/InusedApplicationTests.java
+++ inused/src/test/java/com/c_comachi/inused/InusedApplicationTests.java
@@ -3,7 +3,6 @@ package com.c_comachi.inused;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class InusedApplicationTests {
 
 	@Test
